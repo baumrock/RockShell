6fd43f3c26c314175c9868fa23452417734a022e
BernhardBaumrock <office@baumrock.com>
2022-07-26 10:17:03 +0200

    add db-pull command

---
+++ App/Commands/DbPull.php
@@ -0,0 +1,76 @@
+<?php namespace RockShell;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * $config->rockshell = [
+ *   'remotes' => [
+ *     'staging' => [
+ *       'ssh' => 'user@yourserver.com',
+ *       'dir' => '/path/to/pw/root',
+ *     ],
+ *   ],
+ * ];
+ */
+
+class DbPull extends Command {
+
+  const backupdir = "/site/assets/backups/database/";
+
+  public function config() {
+    $this
+      ->setDescription("Pull a database dump from remote server")
+      ->addArgument("remote", InputArgument::OPTIONAL)
+      ->addOption("list", "l", InputOption::VALUE_NONE, "List remotes")
+      ->addOption("keep", "k", InputOption::VALUE_NONE, "Keep tmp.sql after restore")
+    ;
+  }
+
+  public function handle() {
+    $wire = $this->wire();
+
+    // get remotes from config
+    $remotes = $this->getConfig('remotes');
+    if(!$remotes OR !count($remotes)) {
+      $this->error("No remotes defined in \$config->rockshell['remotes'] - see DbPull.php for help");
+      return self::FAILURE;
+    }
+
+    if($this->option("list")) {
+      $this->info("Available remotes:");
+      $this->write($remotes);
+    }
+
+    // connect to remote
+    $remoteName = $this->argument("remote") ?: $this->choice("Choose remote", array_keys($remotes));
+    $remote = (object)$remotes[$remoteName];
+    $ssh = $remote->ssh;
+    $dir = rtrim($remote->dir, "/");
+    $folder = trim(DbDump::backupdir, "/");
+    $this->write("Creating remote dump...");
+    $this->sshExec($ssh, "cd $dir
+      php RockShell/rockshell db-dump -f tmp.sql");
+
+    $this->write("Copying dump to local...");
+    $this->exec("scp $ssh:$dir/$folder/tmp.sql {$wire->config->paths->root}/$folder/tmp.sql");
+
+    $this->write("Removing remote dump...");
+    $this->sshExec($ssh, "cd $dir && rm -rf $dir/$folder/tmp.sql");
+
+    $this->call("db-restore", [
+      '--y' => true,
+      '--file' => 'tmp.sql',
+    ]);
+
+    if(!$this->option('keep')) {
+      $this->write("Removing tmp.sql...");
+      $this->exec("rm $folder/tmp.sql", false);
+      $this->info("Done");
+    }
+
+    $this->write("");
+    return self::SUCCESS;
+  }
+
+}
