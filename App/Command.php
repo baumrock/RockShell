cbed1d017cd2b6c95dcdbf3e45d75a2820f9d914
BernhardBaumrock <office@baumrock.com>
2022-09-30 18:07:23 +0200

    refactor to psr12

--- App/Command.php
+++ App/Command.php
@@ -1,4 +1,6 @@
-<?php namespace RockShell;
+<?php
+
+namespace RockShell;
 
 use Exception;
 use Illuminate\Console\Command as ConsoleCommand;
@@ -7,7 +9,8 @@ use ReflectionClass;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Command extends ConsoleCommand {
+class Command extends ConsoleCommand
+{
 
   /**
    * Reference to the application
@@ -30,34 +33,38 @@ class Command extends ConsoleCommand {
   /** @var  */
   private $wire;
 
-  public function __construct($name = null) {
+  public function __construct($name = null)
+  {
     $this->reflect = new ReflectionClass($this); // very first!
     $this->name = $this->name($name);
     parent::__construct($this->name);
-    if(method_exists($this, 'config')) $this->config();
+    if (method_exists($this, 'config')) $this->config();
   }
 
   /**
    * Check if a .ddev folder exists in pw root
    * @return bool
    */
-  public function ddev() {
-    return $this->ddevExists() AND is_dir("/var/www/html");
+  public function ddev()
+  {
+    return $this->ddevExists() and is_dir("/var/www/html");
   }
 
   /**
    * Check if a ddev folder exists
    * @return bool
    */
-  public function ddevExists() {
-    return is_dir($this->app->rootPath().".ddev");
+  public function ddevExists()
+  {
+    return is_dir($this->app->rootPath() . ".ddev");
   }
 
   /**
    * Get name of the folder where the current command file lives
    * @return string
    */
-  public function dirName() {
+  public function dirName()
+  {
     return basename(dirname($this->reflect->getFileName()));
   }
 
@@ -70,19 +77,21 @@ class Command extends ConsoleCommand {
    *   touch test2.txt
    * ");
    */
-  public function exec($cmd, $output = true) {
+  public function exec($cmd, $output = true)
+  {
     // replace newlines by && to join multiline commands
     $cmd = str_replace("\n", " && ", $cmd);
-    if($this->output->isVerbose()) $this->write($cmd);
+    if ($this->output->isVerbose()) $this->write($cmd);
     exec($cmd, $out);
-    if($this->output->isVerbose() OR $output) $this->write($out);
+    if ($this->output->isVerbose() or $output) $this->write($out);
     return $out;
   }
 
   /**
    * Overwrite the symfony commands execute() method and proxy it to handle()
    */
-  protected function execute(InputInterface $input, OutputInterface $output) {
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
     $this->input = $input;
     $this->output = $output;
     return $this->handle();
@@ -92,7 +101,8 @@ class Command extends ConsoleCommand {
    * Filter dom for current selector and print output to console
    * @return void
    */
-  public function filter($selector) {
+  public function filter($selector)
+  {
     echo $this->browser->getCrawler()->filter($selector)->outerHtml();
   }
 
@@ -100,13 +110,14 @@ class Command extends ConsoleCommand {
    * Get config from $config->rockshell
    * @return mixed
    */
-  public function getConfig($prop = null, $quiet = true) {
+  public function getConfig($prop = null, $quiet = true)
+  {
     $config = $this->wire()->config->rockshell;
-    if(!$config) return false;
-    if($prop) {
-      if(array_key_exists($prop, $config)) return $config[$prop];
+    if (!$config) return false;
+    if ($prop) {
+      if (array_key_exists($prop, $config)) return $config[$prop];
       else {
-        if($quiet) return false;
+        if ($quiet) return false;
         else return $this->error("Property '$prop' not found in config");
       }
     }
@@ -116,14 +127,16 @@ class Command extends ConsoleCommand {
   /**
    * Execute this command
    */
-  public function handle() {
+  public function handle()
+  {
     throw new LogicException('You must override the handle() method in your command.');
   }
 
   /**
    * Check if domelement has given class
    */
-  public function hasClass($el, $class) {
+  public function hasClass($el, $class)
+  {
     $classes = explode(" ", $el->getAttribute('class'));
     return in_array($class, $classes);
   }
@@ -132,7 +145,8 @@ class Command extends ConsoleCommand {
    * Print the current html to console in interactive mode
    * @return void
    */
-  public function html() {
+  public function html()
+  {
     echo $this->browser->getInternalResponse()->getContent();
   }
 
@@ -142,7 +156,8 @@ class Command extends ConsoleCommand {
    * Taken from PW Sanitizer
    * @return string
    */
-  public static function hyphenCase($value, array $options = array()) {
+  public static function hyphenCase($value, array $options = array())
+  {
 
     $defaults = array(
       'hyphen' => '-',
@@ -155,16 +170,16 @@ class Command extends ConsoleCommand {
     $hyphen = $options['hyphen'];
 
     // if value is empty then exit now
-    if(!strlen($value)) return '';
+    if (!strlen($value)) return '';
 
-    if($options['allowUnderscore']) $options['allow'] .= '_';
+    if ($options['allowUnderscore']) $options['allow'] .= '_';
 
     // check if value is already in the right format, and return it if so
-    if(strtolower($value) === $value) {
-      if($options['allow'] === $defaults['allow']) {
-        if(ctype_alnum(str_replace($hyphen, '', $value))) return $value;
+    if (strtolower($value) === $value) {
+      if ($options['allow'] === $defaults['allow']) {
+        if (ctype_alnum(str_replace($hyphen, '', $value))) return $value;
       } else {
-        if(preg_match('/^[' . $hyphen . $options['allow'] . ']+$/', $value)) return $value;
+        if (preg_match('/^[' . $hyphen . $options['allow'] . ']+$/', $value)) return $value;
       }
     }
 
@@ -179,7 +194,7 @@ class Command extends ConsoleCommand {
     // prevent doubled hyphens
     $value = preg_replace('/' . $hyphen . $hyphen . '+/', $hyphen, $value);
 
-    if($options['allowUnderscore']) {
+    if ($options['allowUnderscore']) {
       $value = str_replace(array('-_', '_-'), '_', $value);
     }
 
@@ -189,11 +204,12 @@ class Command extends ConsoleCommand {
   /**
    * Build name of current command
    */
-  public function name($name) {
+  public function name($name)
+  {
     $name = $name ?: $this->shortName();
     $dir = $this->dirName();
-    $namespace = $dir === 'Commands' ? '' : strtolower($dir).":";
-    return $namespace.$name;
+    $namespace = $dir === 'Commands' ? '' : strtolower($dir) . ":";
+    return $namespace . $name;
   }
 
   /**
@@ -202,8 +218,9 @@ class Command extends ConsoleCommand {
    * @param string $path
    * @return string
    */
-  public static function normalizeSeparators($path) {
-    if(DIRECTORY_SEPARATOR == '/') return $path;
+  public static function normalizeSeparators($path)
+  {
+    if (DIRECTORY_SEPARATOR == '/') return $path;
     $path = str_replace(DIRECTORY_SEPARATOR, '/', $path);
     return $path;
   }
@@ -212,20 +229,21 @@ class Command extends ConsoleCommand {
    * Pause execution of command and enter interactive mode
    * @return void
    */
-  public function pause($vars = []) {
+  public function pause($vars = [])
+  {
     $this->warn("\nEntering interactive shell...");
     $this->write("You can now execute any PHP command");
     $this->write("The current command is available as \$this");
 
     extract($vars);
-    foreach($vars as $k=>$v) {
+    foreach ($vars as $k => $v) {
       $data = $v;
-      if(is_object($data)) $data = class_basename($data);
+      if (is_object($data)) $data = class_basename($data);
       $this->write("  \$$k = $data");
     }
 
     $history = [];
-    while($cmd = $this->askWithCompletion("PHP", $history)) {
+    while ($cmd = $this->askWithCompletion("PHP", $history)) {
       $history[] = $cmd;
       array_reverse($history);
       ob_start();
@@ -235,7 +253,7 @@ class Command extends ConsoleCommand {
         echo $th->getMessage();
       }
       $out = ob_get_clean();
-      if($out) $this->write("$out\n");
+      if ($out) $this->write("$out\n");
     }
   }
 
@@ -251,12 +269,13 @@ class Command extends ConsoleCommand {
    *                         to select from
    * @return string
    */
-  function randomStr($length = 0, $keyspace = null) {
-    if(!$keyspace) $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if(!$length) $length = rand(10, 15);
+  function randomStr($length = 0, $keyspace = null)
+  {
+    if (!$keyspace) $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (!$length) $length = rand(10, 15);
     $str = '';
     $max = mb_strlen($keyspace, '8bit') - 1;
-    if($max < 1) {
+    if ($max < 1) {
       throw new Exception('$keyspace must be at least two characters long');
     }
     for ($i = 0; $i < $length; ++$i) {
@@ -269,9 +288,10 @@ class Command extends ConsoleCommand {
    * Return path of RockShell folder for current command
    * @return string
    */
-  public function shellPath() {
+  public function shellPath()
+  {
     $path = $this->reflect->getFileName();
-    while(basename($path) !== 'RockShell') $path = dirname($path);
+    while (basename($path) !== 'RockShell') $path = dirname($path);
     return "$path/";
   }
 
@@ -279,7 +299,8 @@ class Command extends ConsoleCommand {
    * Get short classname of this command
    * @return string
    */
-  public function shortName() {
+  public function shortName()
+  {
     return $this->hyphenCase($this->reflect->getShortName());
   }
 
@@ -287,7 +308,8 @@ class Command extends ConsoleCommand {
    * Execute command on remote via ssh
    * @return void
    */
-  public function sshExec($ssh, $cmd) {
+  public function sshExec($ssh, $cmd)
+  {
     $cmd = str_replace("\n", " && ", $cmd);
     $this->exec("ssh $ssh \"$cmd\"");
   }
@@ -296,11 +318,12 @@ class Command extends ConsoleCommand {
    * Convert to string
    * @return string
    */
-  public function str($data) {
-    if(is_string($data)) return $data;
-    if(is_array($data)) {
+  public function str($data)
+  {
+    if (is_string($data)) return $data;
+    if (is_array($data)) {
       $out = '';
-      foreach($data as $line) $out .= "  $line\n";
+      foreach ($data as $line) $out .= "  $line\n";
       return $out;
     }
     ob_start();
@@ -313,24 +336,26 @@ class Command extends ConsoleCommand {
    * Does not check if the file exists!
    * @return string
    */
-  public function stub($file) {
+  public function stub($file)
+  {
     // the RockShell module has an App folder
     // all other modules do not have this folder
-    if(is_dir($this->shellPath()."App"))
-      return $this->shellPath()."App/stubs/$file";
+    if (is_dir($this->shellPath() . "App"))
+      return $this->shellPath() . "App/stubs/$file";
     else
-      return $this->shellPath()."stubs/$file";
+      return $this->shellPath() . "stubs/$file";
   }
 
   /**
    * Populate stub placeholders
    * @return void
    */
-  public function stubPopulate($src, $dst, $vars = []) {
+  public function stubPopulate($src, $dst, $vars = [])
+  {
     $content = file_get_contents($src);
     $this->write("Writing $src to $dst");
-    foreach($vars as $k=>$v) {
-      $content = str_replace("{".$k."}", $v, $content);
+    foreach ($vars as $k => $v) {
+      $content = str_replace("{" . $k . "}", $v, $content);
     }
     file_put_contents($dst, $content);
   }
@@ -338,7 +363,8 @@ class Command extends ConsoleCommand {
   /**
    * Write success message to output
    */
-  public function success($str) {
+  public function success($str)
+  {
     $this->info($this->str($str));
   }
 
@@ -347,16 +373,18 @@ class Command extends ConsoleCommand {
    * @param string $path
    * @return string
    */
-  public function trailingSlash($path) {
-    return rtrim($this->normalizeSeparators($path), "/")."/";
+  public function trailingSlash($path)
+  {
+    return rtrim($this->normalizeSeparators($path), "/") . "/";
   }
 
   /**
    * Get wire instance
    * @return Wire
    */
-  public function wire() {
-    if($this->wire) return $this->wire;
+  public function wire()
+  {
+    if ($this->wire) return $this->wire;
     chdir($this->app->rootPath());
     include 'index.php';
     return $this->wire = $wire;
@@ -365,9 +393,9 @@ class Command extends ConsoleCommand {
   /**
    * Write string to output
    */
-  public function write($str) {
+  public function write($str)
+  {
     $str = $this->str($str);
     $this->output->writeln($str);
   }
-
 }
