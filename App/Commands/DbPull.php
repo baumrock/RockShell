0504f013620097ba2a0a30e741d60e4ee1b8fc7a
BernhardBaumrock <office@baumrock.com>
2023-04-08 16:54:06 +0200

    feat: auto-select remote for db-pull

--- App/Commands/DbPull.php
+++ App/Commands/DbPull.php
@@ -1,4 +1,6 @@
-<?php namespace RockShell;
+<?php
+
+namespace RockShell;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,39 +16,48 @@ use Symfony\Component\Console\Input\InputOption;
  * ];
  */
 
-class DbPull extends Command {
+class DbPull extends Command
+{
 
   const backupdir = "/site/assets/backups/database/";
 
-  public function config() {
+  public function config()
+  {
     $this
       ->setDescription("Pull a database dump from remote server")
       ->addArgument("remote",         InputArgument::OPTIONAL)
       ->addOption("list",       "l",  InputOption::VALUE_NONE,      "List remotes")
       ->addOption("keep",       "k",  InputOption::VALUE_NONE,      "Keep tmp.sql after restore")
-      ->addOption("php",        "p",  InputOption::VALUE_OPTIONAL,  "PHP command to use, eg keyhelp-php81")
-    ;
+      ->addOption("php",        "p",  InputOption::VALUE_OPTIONAL,  "PHP command to use, eg keyhelp-php81");
   }
 
-  public function handle() {
+  public function handle()
+  {
     $wire = $this->wire();
 
     // get remotes from config
     $remotes = $this->getConfig('remotes');
-    if(!$remotes OR !count($remotes)) {
+    if (!$remotes or !count($remotes)) {
       $this->error("No remotes defined in \$config->rockshell['remotes'] - see DbPull.php for help");
       return self::FAILURE;
     }
 
-    if($this->option("list")) {
+    if ($this->option("list")) {
       $this->info("Available remotes:");
       $this->write($remotes);
     }
 
+    // if remote is not specified and we only have one remote
+    // we take that one to pull from
+    if (!$this->argument("remote") and count($remotes) === 1) {
+      foreach ($remotes as $remote) {
+        $remote = (object)$remote;
+      }
+    } else {
+      $remoteName = $this->argument("remote") ?: $this->choice("Choose remote", array_keys($remotes));
+      $remote = (object)$remotes[$remoteName];
+    }
 
-    // connect to remote
-    $remoteName = $this->argument("remote") ?: $this->choice("Choose remote", array_keys($remotes));
-    $remote = (object)$remotes[$remoteName];
     $ssh = $remote->ssh;
     $dir = rtrim($remote->dir, "/");
     $folder = trim(DbDump::backupdir, "/");
@@ -67,7 +78,7 @@ class DbPull extends Command {
       '--file' => 'tmp.sql',
     ]);
 
-    if(!$this->option('keep')) {
+    if (!$this->option('keep')) {
       $this->write("Removing tmp.sql...");
       $this->exec("rm $folder/tmp.sql", false);
       $this->info("Done");
@@ -76,5 +87,4 @@ class DbPull extends Command {
     $this->write("");
     return self::SUCCESS;
   }
-
 }
