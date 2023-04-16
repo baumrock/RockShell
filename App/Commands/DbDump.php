9b193ef782799447d6bfc98de5333f156753a493
BernhardBaumrock <office@baumrock.com>
2023-04-16 19:28:38 +0200

    feat: add filesize on dump log

--- App/Commands/DbDump.php
+++ App/Commands/DbDump.php
@@ -1,29 +1,44 @@
-<?php namespace RockShell;
+<?php
+
+namespace RockShell;
 
 use Symfony\Component\Console\Input\InputOption;
 
-class DbDump extends Command {
+use function ProcessWire\wireBytesStr;
+
+class DbDump extends Command
+{
 
   const backupdir = "/site/assets/backups/database/";
 
-  public function config() {
+  public function config()
+  {
     $this
       ->setDescription("Create a database dump using PW backup tools")
-      ->addOption("file", "f", InputOption::VALUE_OPTIONAL,
-        "Filename to be used for the dump inside ".self::backupdir, "db.sql")
-      ->addOption("delete", "d", InputOption::VALUE_NONE,
-        "Delete all users except guest")
-    ;
+      ->addOption(
+        "file",
+        "f",
+        InputOption::VALUE_OPTIONAL,
+        "Filename to be used for the dump inside " . self::backupdir,
+        "db.sql"
+      )
+      ->addOption(
+        "delete",
+        "d",
+        InputOption::VALUE_NONE,
+        "Delete all users except guest"
+      );
   }
 
-  public function handle() {
+  public function handle()
+  {
     $wire = $this->wire();
     $file = $this->option("file");
 
     // delete all users except guest
-    if($this->option("delete")) {
-      foreach($wire->users as $user) {
-        if($user->name == 'guest') continue;
+    if ($this->option("delete")) {
+      foreach ($wire->users as $user) {
+        if ($user->name == 'guest') continue;
         $this->write("Deleting user {$user->name}...");
         $wire->users->delete($user);
       }
@@ -34,13 +49,15 @@ class DbDump extends Command {
     $backup = $wire->database->backups();
     $backup->setDatabaseConfig($wire->config);
     $file = $backup->backup(['filename' => $file]);
-    if($file) {
-      $this->success("Backed up to: $file\n");
+    if ($file) {
+      $this->success("Backed up to: $file");
+      $size = wireBytesStr($file, true);
+      $this->write("Filesize: $size");
+      $this->write("");
     } else {
-      $this->success("Backup failed: " . implode("<br>", $backup->errors())."\n");
+      $this->success("Backup failed: " . implode("<br>", $backup->errors()) . "\n");
     }
 
     return self::SUCCESS;
   }
-
 }
