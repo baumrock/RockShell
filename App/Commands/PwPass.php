4d0c53af6aee75f628003b4dc22af58588dcc904
BernhardBaumrock <office@baumrock.com>
2023-04-16 18:29:59 +0200

    feat: add PwUser and PwPass

---
+++ App/Commands/PwPass.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RockShell;
+
+use ProcessWire\WireRandom;
+use Symfony\Component\Console\Input\InputOption;
+
+class PwPass extends Command
+{
+
+  public function config()
+  {
+    $this
+      ->setDescription("Reset passwort of a user")
+      ->addOption("user", "u", InputOption::VALUE_OPTIONAL, "username");
+  }
+
+  public function handle()
+  {
+    if (!$user = $this->option('user')) {
+      $users = [];
+      foreach ($this->wire()->users as $u) $users[] = $u->name;
+      $user = $this->choice("Select user", $users);
+    }
+
+    $rand = new WireRandom();
+    $pass = $rand->alphanumeric(0, [
+      'maxLength' => 20,
+      'minLength' => 12,
+    ]);
+    $pass = $this->ask("Enter Password", $pass);
+
+    $user = $this->wire()->users->get("name=$user");
+    $user->setAndSave('pass', $pass);
+
+    $this->success("Password set for user " . $user->name . ".");
+    return self::SUCCESS;
+  }
+}
---
+++ App/Commands/PwUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RockShell;
+
+class PwUsers extends Command
+{
+
+  public function config()
+  {
+    $this
+      ->setDescription("List all users");
+  }
+
+  public function handle()
+  {
+    foreach ($this->wire()->users as $u) {
+      $this->write("{$u->name} ($u)");
+    }
+    return self::SUCCESS;
+  }
+}
