161f661d2014320418b3aafc3efa5091523bb4c9
BernhardBaumrock <office@baumrock.com>
2023-05-23 17:21:01 +0200

    feat: add command user-delete

---
+++ App/Commands/UserDelete.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace RockShell;
+
+use ProcessWire\WireRandom;
+use Symfony\Component\Console\Input\InputOption;
+
+class UserDelete extends Command
+{
+
+  public function config()
+  {
+    $this
+      ->setDescription("Delete a user")
+      ->addOption("user", "u", InputOption::VALUE_OPTIONAL, "username");
+  }
+
+  public function handle()
+  {
+    if (!$user = $this->option('user')) {
+      $users = [];
+      foreach ($this->wire()->users as $u) $users[] = $u->name;
+      $user = $this->choice("Select user", $users);
+    }
+
+    $user = $this->wire()->users->get("name=$user");
+    $this->wire()->users->delete($user);
+
+    $this->success("{$user->name} has been deleted.");
+    return self::SUCCESS;
+  }
+}
