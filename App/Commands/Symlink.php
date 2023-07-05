9081b12b2bf9495f5c48e1664786429d87c24c7e
BernhardBaumrock <office@baumrock.com>
2023-07-05 16:54:56 +0200

    fix: rename rockshell symlink

--- App/Commands/Symlink.php
+++ App/Commands/Symlink.php
@@ -2,21 +2,27 @@
 
 namespace RockShell;
 
+/**
+ * Create a symlink to be able to call RockShell directly from the pw root directory.
+ * We call the symlink "rock" because RockShell is already taken by the folder
+ * and that leads to conflicts on case-insensitive file systems.
+ */
 class Symlink extends Command
 {
 
   public function config()
   {
-    $this->setDescription("Creates a rockshell symlink in the PW root folder");
+    $this->setDescription("Creates a symlink to RockShell in the PW root folder");
   }
 
   public function handle()
   {
     $root = $this->wire()->getRootPath();
-    $this->exec("cd $root && ln -snf RockShell/rockshell rockshell", false);
-    $dst = "{$root}rockshell";
-    if (is_file($dst)) $this->success("Symlink created at $dst");
-    else $this->error("Error creating symlink at $dst");
+    $this->exec("cd $root && ln -snf RockShell/rockshell rock", false);
+    if (is_file($root . "rock")) {
+      $this->success("Symlink 'rock' created at $root");
+      $this->comment("You can now use 'php rock' to execute RockShell");
+    } else $this->error("Error creating 'rock' symlink at $root");
     return self::SUCCESS;
   }
 }
