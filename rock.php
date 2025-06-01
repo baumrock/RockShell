1723a8fc09e91c3f9cd6cf801ed4daaa48182598
BernhardBaumrock <office@baumrock.com>
2025-06-01 16:54:50 +0200

    fix: remove try/catch to make development easier https://processwire.com/talk/topic/28751-rockshell-a-processwire-commandline-companion-%E2%8C%A8%EF%B8%8F/?do=findComment&comment=248651

--- rock.php
+++ rock.php
@@ -9,14 +9,10 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 require __DIR__ . '/vendor/autoload.php';
 $app = new \RockShell\Application();
 $app->registerCommands();
-try {
-  if (version_compare(phpversion(), '8.0', '<')) {
-    throw new Exception(
-      "PHP Version must be at least 8.0 - your version is " . phpversion()
-    );
-  }
-  $app->run();
-} catch (\Throwable $th) {
+$minVersion = '8.0';
+if (version_compare(phpversion(), $minVersion, '<')) {
   $output = new ConsoleOutput();
-  $output->writeln("<bg=red;options=bold>" . $th->getMessage() . "</>");
+  $output->writeln("<bg=red;options=bold>PHP Version must be at least $minVersion - your version is " . phpversion() . "</>");
+  exit(1);
 }
+$app->run();
